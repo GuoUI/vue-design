0ca29c6a19ef61ff2c8245ad351fc71e7fd93786
guojun <2650744839@qq.com>
2021-09-24 10:28:41 +0800

    修改脚本

--- build/deploy.sh
+++ build/deploy.sh
@@ -7,11 +7,11 @@ npm run build
 cd docs/.vuepress/dist
 
 git init
-git config user.name 'HcySunYang'
-git config user.email 'HcySunYang@outlook.com'
+git config user.name 'GuoUI'
+git config user.email 'guoheiter@outlook.com'
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:HcySunYang/vue-design.git master:gh-pages
+git push -f https://github.com/GuoUI/vue-design.git master:gh-pages
 
 cd -
